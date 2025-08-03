d43d7a6920c45a261eaa6ade0625a2f5cbf4196d
Hauke Schnau <hauke@schnau-lilienthal.de>
2025-08-03 16:53:37 +0200

    Add vimtex and lazygit plugins

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
+map("n", "<leader>cc", ":VimtexCompile<CR>")
+map("n", "<leader>cv", ":VimtexView<CR>")
+map("n", "<leader>cq", ":VimtexStop<CR>")
+
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,41 @@ return {
     end,
   },
 
+  {
+    "lervag/vimtex",
+    ft = { "tex" },
+    init = function()
+      vim.g.tex_flavor = "latex"
+      vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_mappings_enabled = 0
+      vim.g.vimtex_indent_enabled = 0
+
+      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_context_pdf_viewer = "zathura"
+    end,
+  },
+
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+        "LazyGit",
+        "LazyGitConfig",
+        "LazyGitCurrentFile",
+        "LazyGitFilter",
+        "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+        { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit" }
+    }
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
